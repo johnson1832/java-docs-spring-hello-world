85fd44e37467f1baf289d06d15ab5333d09b61a9
a028152 <David.Johnson@alliancedata.com>
2022-11-02 13:59:51 -0400

    Logger

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApplication {
 
+	Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 	@RequestMapping("/")
 	String sayHello() {
+		logger.debug("Hello");
 		return "Hello Azure X!";
 	}
 }
