59f976beccc331c73eedbeba43aaa5da0e611b34
a028152 <David.Johnson@alliancedata.com>
2022-11-02 15:20:17 -0400

    config mgr poc

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.azure.data.appconfiguration.ConfigurationClient;
+import com.azure.data.appconfiguration.ConfigurationClientBuilder;
+import com.azure.data.appconfiguration.models.ConfigurationSetting;
+
 @SpringBootApplication
 @RestController
 public class DemoApplication {
@@ -19,11 +23,21 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	String sayHello() {
-		logger.debug("Hello");
+		logger.debug("************************************* Start");
 		
-		String value1 = System.getenv("TEST_CONFIG1");
-		String value2 = System.getenv("TEMP_STRING");
+		ConfigurationClient configurationClient = new ConfigurationClientBuilder()
+			    .connectionString("Endpoint=https://appconf-ncus-dev-apim-001.azconfig.io;Id=hoKr-l7-s0:q+4N3KLIQsVMXv87bgmr;Secret=0Z9olEgIxz7ZMLnAVU+sPybQM2WKKq5WUA3J5x6jP6o=")
+			    .buildClient();
 		
-		return "Hello Azure : {" + value1 + "," + value2 + "}!";
+		String retrievedSetting = configurationClient.getConfigurationSetting("TEMP_STRING", "").getValue(); //config manager (local = xxx)
+		String retrievedSetting2 = configurationClient.getConfigurationSetting("TEST_CONFIG1", "").getValue(); //config manager (local = xxx)
+
+		String value2 = System.getenv("TEMP_STRING"); //app and config manager (local = null)
+		String value3 = System.getenv("TEMP_STRING2"); //app (local = null) 
+		String value4 = System.getenv("TEST_CONFIG1");  //config manager (local = null)
+
+		logger.debug("************************************* Return");
+		
+		return "Hello Azure : {" + retrievedSetting + "," + value2 + "," + value3 + "," + value4 + "," + retrievedSetting2 + "}!";
 	}
 }
